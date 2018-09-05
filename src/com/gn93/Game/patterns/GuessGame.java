03b1a5754ae928308d6477fc7639f787e8b27413
GN93 <gnajduch@gmail.com>
2018-09-05 16:16:33 +0200

    Work in progress

--- src/com/gn93/Game/patterns/GuessGame.java
+++ src/com/gn93/Game/patterns/GuessGame.java
@@ -1,5 +1,8 @@
 package com.gn93.Game.patterns;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class GuessGame {
 
     public GuessGame() {
@@ -7,6 +10,8 @@ public class GuessGame {
 
     private int score;
     private static GuessGame instance;
+    private Scanner sc = new Scanner(System.in);
+    private Random rn = new Random();
 
     public int getScore() {
         return score;
@@ -21,7 +26,15 @@ public class GuessGame {
         return instance;
     }
 
-    public void play(){
+    public void play(Scanner sc, Random rn){
+
+        for (int i = 0; i < 10; i++) {
+            int guess = sc.nextInt();
+            if(guess == rn.nextInt(9)){
+                score++;
+                System.out.println("Brawo! Twoj wynik =" + score);
+            }
+        }
 
     }
 
