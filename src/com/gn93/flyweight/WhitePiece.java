077199661096925b267986a612139978be95b906
Gabriel Najduch <gnajduch@gmail.com>
2018-10-01 20:18:45 +0200

    Flyweight design pattern done.

---
+++ src/com/gn93/flyweight/WhitePiece.java
@@ -0,0 +1,8 @@
+package com.gn93.flyweight;
+
+public class WhitePiece extends ChessPiece {
+
+    public WhitePiece(String name, int numberPosition, String letterPosition) {
+        super(name, numberPosition, letterPosition, "white");
+    }
+}
